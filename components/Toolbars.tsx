cd8d470f481de758ddec6f0c44255aa33f7bfe32
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-05-21 18:21:47 +0530

    added

--- components/Toolbars.tsx
+++ components/Toolbars.tsx
@@ -1,7 +1,7 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './ui/button';
-import { FORMAT_TEXT_COMMAND } from 'lexical';
+import { FORMAT_TEXT_COMMAND, $getSelection, $isRangeSelection, SELECTION_CHANGE_COMMAND } from 'lexical';
 
 const Toolbars = () => {
     const [editor] = useLexicalComposerContext();
@@ -10,11 +10,37 @@ const Toolbars = () => {
     const [isUnderline, setIsUnderline] = useState(false);
     const [isStrikethrough, setIsStrikethrough] = useState(false);
 
+    const updateToolbar = useCallback(() => {
+        const selection = $getSelection();
+        if ($isRangeSelection(selection)) {
+            setIsBold(selection.hasFormat('bold'));
+            setIsItalic(selection.hasFormat('italic'));
+            setIsUnderline(selection.hasFormat('underline'));
+            setIsStrikethrough(selection.hasFormat('strikethrough'));
+        }
+    }, []);
+
+    useEffect(() => {
+        editor.registerUpdateListener(({ editorState }) => {
+            editorState.read(() => {
+                updateToolbar();
+            });
+        });
+        return editor.registerCommand(
+            SELECTION_CHANGE_COMMAND,
+            () => {
+                updateToolbar();
+                return false;
+            },
+            1
+        );
+    }, [editor, updateToolbar]);
+
     return (
-        <div className='flex gap-2'>
+        <div className='flex items-center'>
+            <div className='flex gap-2'>
             <Button 
                 onClick={() => {
-                    setIsBold(!isBold);
                     editor.dispatchCommand(FORMAT_TEXT_COMMAND, "bold");
                 }} 
                 variant="ghost"
@@ -22,7 +48,6 @@ const Toolbars = () => {
                 size="sm">B</Button>
             <Button 
                 onClick={() => {
-                    setIsItalic(!isItalic);
                     editor.dispatchCommand(FORMAT_TEXT_COMMAND, "italic");
                 }} 
                 variant="ghost"
@@ -30,7 +55,6 @@ const Toolbars = () => {
                 size="sm">I</Button>
             <Button 
                 onClick={() => {
-                    setIsUnderline(!isUnderline);
                     editor.dispatchCommand(FORMAT_TEXT_COMMAND, "underline");
                 }} 
                 variant="ghost"
@@ -38,12 +62,17 @@ const Toolbars = () => {
                 size="sm">U</Button>
             <Button 
                 onClick={() => {
-                    setIsStrikethrough(!isStrikethrough);
                     editor.dispatchCommand(FORMAT_TEXT_COMMAND, "strikethrough");
                 }} 
                 variant="ghost"
                 className={`${isStrikethrough ? 'bg-neutral-700 hover:bg-neutral-600' : ''}`}
                 size="sm">S</Button>
+            </div>
+            <div className='ml-5 flex gap-2'>
+            <Button 
+                variant="ghost"
+                size="sm">h1</Button>
+            </div>
         </div>
     )
 }
